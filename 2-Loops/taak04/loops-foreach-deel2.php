6888eb1ba8206026e1a544fafd09f4a621ca7f92
Meshano Fransman <shano.fransman@student.rocva.nl>
2020-06-29 20:30:10 +0200

    Nagekeken

--- 2-Loops/taak04/loops-foreach-deel2.php
+++ 2-Loops/taak04/loops-foreach-deel2.php
@@ -1,12 +1,21 @@
-<?php
-$klassen = array("8A", "8B", "8C", "8D", "8E", "9A", "9B", "9C", "9D", "9E");
-
-echo "Ik zit in klas:  <select name=cars id=carss>";
-echo "<input type=submit>";
-"<select id=cars>
-<option value=volvo>Volvo</option>
-<option value=saab>Saab</option>
-<option value=opel>Opel</option>
-<option value=audi>Audi</option>
-</select>";
-?>      
+<!DOCTYPE html>
+<html>
+<body>
+<form action="/action_page.php">
+  <label for="cars">Ik zit in klas:</label>
+  <select name="cars" id="cars">
+    <option value="volvo">8A</option>
+    <option value="saab">8B</option>
+    <option value="opel">8C</option>
+    <option value="audi">8D</option>
+    <option value="volvo">9A</option>
+    <option value="saab">9B</option>
+    <option value="opel">9C</option>
+    <option value="audi">9D</option>
+    <option value="audi">9E</option>
+  </select>
+  <br><br>
+  <input type="submit" value="verzend">
+</form>
+</body>
+</html>
--- 4-Functions/taak02/functies-2.php
+++ 4-Functions/taak02/functies-2.php
@@ -1,7 +1,33 @@
 <?php
-echo "<ul>Als ik de getallen 4, 2 en 9 bij elkaar optel, is de uitkomst 15</ul>";
+function rekenUit($c1, $c2, $c3) {
+
+	$som = $c1 . " " . $c2 . " " . $c3;
+  
+	return $som;
+
+}
+$helesom = rekenUit("Als ik de getallen 4," , "2 en 9 bij elkaar optel," , "is de uitkomst 15");
+echo $helesom;
 echo "<br>";
-echo "<ul>Als ik de getallen 1, 2 en 3 bij elkaar optel, is de uitkomst 6</ul>";
 echo "<br>";
-echo "<ul>Als ik de getallen 40, 20 en 91 bij elkaar optel, is de uitkomst 159</ul>";
+function rekenUit2($c4, $c2, $c6) {
+
+	$som = $c4 . " " . $c2 . " " . $c6;
+  
+	return $som;
+
+}
+$helesom = rekenUit2("Als ik de getallen 1," , "2 en 9 bij elkaar optel," , "is de uitkomst 6");
+echo $helesom;
+echo "<br>";
+echo "<br>";
+function rekenUit3($c5, $c8, $c9) {
+
+	$som = $c5 . " " . $c8 . " " . $c9;
+  
+	return $som;
+
+}
+$helesom = rekenUit3("Als ik de getallen 40," , "28 en 91 bij elkaar optel," , "is de uitkomst 159");
+echo $helesom;
 ?>
